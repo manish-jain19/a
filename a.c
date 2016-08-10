2193c43da0b6ada326c85f402a766c85aa72239e
sushant <yadavsushant321@gmail.com>
2016-08-10 14:51:38 +0530

    Initial commit

---
+++ a.c
@@ -0,0 +1,11 @@
+/* hello */
+#include<stdio.h>
+int main()
+{
+   int a, b, c;
+   printf("\n\tEnter two numbers to add\n");
+   scanf("\t%d\t%d",&a,&b);
+   c = a + b;
+   printf("\n\tSum of entered numbers = %d\n",c);
+   return 0;
+}
